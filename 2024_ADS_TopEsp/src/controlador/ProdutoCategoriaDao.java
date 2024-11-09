a0b5f90e19391646b54fdc7c6e49d3091cf32eb4
Gabriel Fiaux <gfiauxreis@gmail.com>
2024-11-09 20:23:35 -0300

    package
    criado + 1 package

--- 2024_ADS_TopEsp/src/controlador/ProdutoCategoriaDao.java
+++ 2024_ADS_TopEsp/src/controlador/ProdutoCategoriaDao.java
@@ -4,6 +4,7 @@
  */
 package controlador;
 
+import controlador.conexao.Conexao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
--- 2024_ADS_TopEsp/src/controlador/UsuarioDao.java
+++ 2024_ADS_TopEsp/src/controlador/UsuarioDao.java
@@ -1,5 +1,6 @@
 package controlador;
 
+import controlador.conexao.Conexao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
